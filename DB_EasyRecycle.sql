4ebdb3309ee15ec2664ecc17191ecc0eff50a71f
AndreyFSouza <127605793+Andrey-de-Freitas-Souza@users.noreply.github.com>
2025-04-21 20:04:41 -0300

    Update

---
+++ DB_EasyRecycle.sql
@@ -0,0 +1,24 @@
+create database EasyRecycle;
+
+use EasyRecycle;
+
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,                         		-- Chave primária, com auto incremento
+    `name` VARCHAR(200) NOT NULL,                                 	-- Nome do usuário, não pode ser nulo
+    email VARCHAR(255) NOT NULL UNIQUE,                           	-- Email único, tamanho padrão para emails
+    `password` VARCHAR(255) NOT NULL,                          		-- Senha criptografada, pode ter tamanho variável
+    phone VARCHAR(15),                                          	-- Número de telefone, considerando o formato internacional
+    cep VARCHAR(8),                                              	-- CEP armazenado como string
+    points INT DEFAULT 0,                                         	-- Pontuação, padrão 0
+    `status` VARCHAR(20) DEFAULT 'active',                    		-- Status do usuário (ativo ou inativo), padrão 'active'
+    birth_date DATE,										 		-- Data de nascimento
+    registration_date DATETIME DEFAULT CURRENT_TIMESTAMP,         	-- Data de cadastro, padrão para o momento atual
+    last_login_date DATETIME                                     	-- Data do último login
+);
+
+
+
+
+insert into teste (Nome, Dt_nascimento) values ("Andrey", "2005-06-06");
+insert into teste (Nome, Dt_nascimento) values ("Bete", "1972-04-20");
+select * from teste;
